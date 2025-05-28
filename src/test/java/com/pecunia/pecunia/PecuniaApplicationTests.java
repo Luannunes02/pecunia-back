c8a5c8523fd4402dc4439dcc26075fdf81eace71
Luannunes02 <nunesesbaltar.luan02@gmail.com>
2025-05-28 10:14:32 -0300

    Criado projeto inicial

---
+++ src/test/java/com/pecunia/pecunia/PecuniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pecunia.pecunia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecuniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
