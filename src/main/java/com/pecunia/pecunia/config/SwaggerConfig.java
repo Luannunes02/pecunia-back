ee645cf0351ea84d9c00a2c2709c5a4bf8b90088
Luannunes02 <nunesesbaltar.luan02@gmail.com>
2025-05-28 10:26:58 -0300

    Configurado o Swagger

---
+++ src/main/java/com/pecunia/pecunia/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.pecunia.pecunia.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("API Pecunia")
+            .version("1.0")
+            .description("API do sistema Pecunia")
+            .contact(new Contact()
+                .name("Equipe Pecunia")
+                .email("contato@pecunia.com"))
+            .license(new License()
+                .name("Apache 2.0")
+                .url("http://springdoc.org")));
+  }
+}
