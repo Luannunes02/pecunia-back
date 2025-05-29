ea2e7f3a2fd5097a0988f4b7fc5564e7e225060d
Luannunes02 <nunesesbaltar.luan02@gmail.com>
2025-05-28 21:14:16 -0300

    Resolvido erro de configuração no swagger

--- src/main/java/com/pecunia/pecunia/config/SwaggerConfig.java
+++ src/main/java/com/pecunia/pecunia/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.pecunia.pecunia.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,17 +14,27 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
   @Bean
-  public OpenAPI customOpenAPI() {
+  public OpenAPI openAPI() {
     return new OpenAPI()
         .info(new Info()
-            .title("API Pecunia")
-            .version("1.0")
-            .description("API do sistema Pecunia")
+            .title("Pecunia API")
+            .description("API para gerenciamento de finanças pessoais")
+            .version("1.0.0")
             .contact(new Contact()
-                .name("Equipe Pecunia")
+                .name("Pecunia Team")
                 .email("contato@pecunia.com"))
             .license(new License()
                 .name("Apache 2.0")
-                .url("http://springdoc.org")));
+                .url("http://www.apache.org/licenses/LICENSE-2.0")))
+        .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+        .components(new Components()
+            .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+  }
+
+  private SecurityScheme createAPIKeyScheme() {
+    return new SecurityScheme()
+        .type(SecurityScheme.Type.HTTP)
+        .bearerFormat("JWT")
+        .scheme("bearer");
   }
 }
